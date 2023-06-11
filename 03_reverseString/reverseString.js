37a5c2e8b7414e6f8290f35ed088146b5e09ee8c
Zizu <ziblimzulka.zz@gmail.com>
2023-06-11 16:23:59 +0700

    Reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let chars = str.split('');
+    let newChar = [];
+    if(str === '') 
+        return '';
+    for(let i = chars.length - 1; i >= 0; i--){
+        newChar.push(chars[i]);
+    }
+    return newChar.join('');
 };
 
 // Do not edit below this line
