998c560fd2b1fd9550f4b7857c923ab58decda7f
Zizu <ziblimzulka.zz@gmail.com>
2023-06-11 10:05:38 +0700

    Edit repeatString.js and pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let newString = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    if(str === ''){
+        return '';
+    }
+    for(let i = 0; i < num; i++){
+        newString += str;
+    }
+    return newString;
 };
 
 // Do not edit below this line
