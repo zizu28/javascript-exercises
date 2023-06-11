e1332ff48e44c4da3c9e67183e7c69ad67ac3b9c
Zizu <ziblimzulka.zz@gmail.com>
2023-06-11 18:24:57 +0700

    Convert temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  if(fahrenheit === 32)
+    return 0;
+  let temp = (fahrenheit - 32) * (5 /9);
+  return Math.round(temp * 10)/10;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  if(celsius === 0)
+    return 32;
+  let temp = (celsius * (9 / 5)) + 32;
+  return Math.round(temp * 10) /10;
 };
 
 // Do not edit below this line
