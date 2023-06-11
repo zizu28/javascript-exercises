3e93026b51f462caaf37b829b2323c9628472572
Zizu <ziblimzulka.zz@gmail.com>
2023-06-11 17:17:06 +0700

    Sum all parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(val1, val2) {
+    let result = 0;
+    if(val1 < 0 || typeof val2 !== 'number')
+        return 'ERROR';
+    if(val1 < val2){
+        for(i = val1; i <= val2; i++){
+            result += i;
+        }
+    }
+    else{
+        for(i = val1; i >= val2; i--){
+            result += i;
+        }
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
