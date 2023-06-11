bf43e97e87c956b930f47a73c626fe0fffbd4ad7
Zizu <ziblimzulka.zz@gmail.com>
2023-06-11 16:54:49 +0700

    Remove array elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, val1, val2, val3, val4) {
+    if(isNaN(val2)){
+        let result = arr.filter(a => a !== val1);
+        return result;
+    }
+    let result = arr.filter(a => a !== val1 && a !== val2 && a !== val3 && a !== val4);
+    return result;
 };
 
 // Do not edit below this line
