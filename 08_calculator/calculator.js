9d2de3f3ed1bbd229ee884f3612cca173ebdccb9
Zizu <ziblimzulka.zz@gmail.com>
2023-08-03 02:01:48 +0700

    Calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  return a.reduce((total, curr) => total + curr, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(...values) {
+  let product = 1;
+  for (let i = 0; i < values.length; i++) {
+    product *= values[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a === 0) {
+    return 1
+  }
+  return a * factorial(a-1)
 };
 
 // Do not edit below this line
@@ -29,5 +36,6 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
+  testPathPattern: '~/javascript-exercises/08_calculator/.*\\.spec\\.js$'
 };
