07e496a32946cf0825f6febaff99f17b950cacd2
Zizu <ziblimzulka.zz@gmail.com>
2023-08-03 04:00:40 +0700

    Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) return 'OOPS'
+    const fibs = [0, 1]
+    for(let i = 1; i < num; i++){
+        fibs.push(fibs[i] + fibs[i - 1])
+    }
+    return fibs[num]
 };
 
 // Do not edit below this line
