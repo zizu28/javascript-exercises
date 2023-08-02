7a0a20fc0087da1a4c2d338d1fc8209b96caaf4f
Zizu <ziblimzulka.zz@gmail.com>
2023-08-03 03:09:26 +0700

    Palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
+const palindromes = function(str) {
+    let palWord = str.toLowerCase().replace(/[^a-z0-9]/g, "")
+    return palWord.split("").reverse().join("") === palWord
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
